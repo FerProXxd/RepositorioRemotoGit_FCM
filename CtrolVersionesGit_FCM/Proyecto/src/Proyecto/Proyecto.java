f9a92269ffb4e751bc6c382769ae3a27be7cb50c
Fernando <Fernando@192.168.32.66>
2026-02-24 16:14:48 +0100

    Rama1

--- CtrolVersionesGit_FCM/Proyecto/src/Proyecto/Proyecto.java
+++ CtrolVersionesGit_FCM/Proyecto/src/Proyecto/Proyecto.java
@@ -4,7 +4,14 @@ public class Proyecto {
 
 	public static void main(String[] args) {
 		 System.out.println("Hola este es el principio del proyecto");
-
+		 
+		 System.out.println("Mensaje de rama1");
+		 System.out.println("Mensaje de rama1");
+		 
+		 System.out.println("Mensaje de rama1");
+		 System.out.println("Mensaje de rama1");
+		 
+		 System.out.println("Mensaje de rama1 tarde");
 	}
 
 }
